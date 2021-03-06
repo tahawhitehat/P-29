3e547668f5af01c43a6747f7332250cf63899ecc
tahawhitehat <79064236+tahawhitehat@users.noreply.github.com>
2021-03-06 10:58:34 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+function preload() {
+
+}
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+}
+
+function draw(){
+    Engine.update(engine);
+    ground.display();
+}
